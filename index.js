a9962dfca3e150bc725465c9ccbf594f8b261089
maftuna <maftunaa@gmail.com>
2025-11-10 17:29:28 +0500

    ko'ylaklar boti yaratildi

---
+++ index.js
@@ -0,0 +1,5 @@
+import TelegramBot from "node-telegram-bot-api";
+const TOKEN = "8521415936:AAHswX24cDRDhEPP9GuwDlNFhNfBOwK8z8U";
+const bot = new TelegramBot (TOKEN, {polling:true});
+
+const dressPhotoURL = ""
