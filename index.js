5d6c408299ddd016e83f7473b80f657012b06a87
maftuna <maftunaa@gmail.com>
2025-11-10 17:41:29 +0500

    start bosilgansa first name bn assalomu alaykum deyilishi qoshildi

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const TOKEN = "8521415936:AAHswX24cDRDhEPP9GuwDlNFhNfBOwK8z8U";
 const bot = new TelegramBot (TOKEN, {polling:true});
 
 const dressPhotoURL = ""
+bot.on("message", async function (msg) {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    const firstname = msg.chat.first_name;
+    if(text == "/start") {
+        bot.sendMessage(chatId,`assalomu alaykum, ${firstname}`),{
+            
+        }
+    }
+    
+})
